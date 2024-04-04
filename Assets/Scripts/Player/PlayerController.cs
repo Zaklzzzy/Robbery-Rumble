c70fa1b081271508cec9b69d3db60eeeae2c2845
Zaklzzzy <zaletin123@gmail.com>
2024-04-04 10:48:25 +0300

    feat: add ragdoll without grab and jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,35 +1,23 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] private float _walkSpeed = 15f;
-    [SerializeField] private float _turnSpeed = 10f;
-    [SerializeField, Range(0f,1f)] private float _velocityMultiply;
+    [SerializeField] private float _moveSpeed = 5f;
     [SerializeField] private Animator _animator;
-    private Rigidbody _rb;
-
-    [Header("Jump")]
-    [SerializeField] private float _jumpPower;
-    [SerializeField] private LayerMask _groundLayer;
-    [SerializeField] private Transform _groundCheck;
-    [SerializeField] private float _groundCheckDistance = 0.2f;
-    [SerializeField] private float _gravityMultiplier = 2.5f;
-    public bool _isGrounded = false;
+    [SerializeField] private ConfigurableJoint _cj;
 
     //New Input System
     private GameInput _gameInput;
-    //private IControllable _controllable;
-
-    private Camera _mainCamera;
+    //private IControllable _controllable; 
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
-
-        _mainCamera = Camera.main;
+        _animator = GetComponent<Animator>();
 
         _gameInput = new GameInput();
         _gameInput.Enable();
@@ -37,61 +25,22 @@ public class PlayerController : MonoBehaviour
         //_controllable = GetComponent<IControllable>();
     }
 
-    private void Update()
-    {
-        CheckGround();
-    }
-
     private void FixedUpdate()
     {
-        //Input
-        Vector2 inputDirection = _gameInput.Gameplay.Movement.ReadValue<Vector2>();
+        var inputDirection = _gameInput.Gameplay.Movement.ReadValue<Vector2>();
 
-        float x = inputDirection.x;
+        float x = -inputDirection.x;
         float z = inputDirection.y;
+        Vector3 move = new Vector3(x, 0, z).normalized;
 
-        if (inputDirection != Vector2.zero)
+        if (move != Vector3.zero)
         {
-            //Rotate
-            float targetAngle = Mathf.Atan2(x, z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
-            Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
-            _rb.MoveRotation(Quaternion.Lerp(_rb.rotation, targetRotation, _turnSpeed * Time.deltaTime));
-
-            //Movement
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            _rb.velocity = new Vector3(moveDirection.x * _walkSpeed, _rb.velocity.y, moveDirection.z * _walkSpeed);
-        }
-        else
-        {
-            //Stop
-            _rb.velocity = new Vector3(_rb.velocity.x * _velocityMultiply, _rb.velocity.y, _rb.velocity.z * _velocityMultiply);
+            Quaternion newRotation = Quaternion.LookRotation(move);
+            _cj.targetRotation = newRotation;
         }
 
-        if (!_isGrounded && _rb.velocity.y < 0)
-        {
-            Vector3 extraGravityForce = (Physics.gravity * _gravityMultiplier) - Physics.gravity;
-            _rb.AddForce(extraGravityForce);
-        }
-
-        //Animate
-        _animator.SetFloat("Horizontal", x);
-        _animator.SetFloat("Vertical", z);
-    }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (_isGrounded && context.performed)
-        {
-            _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
-            _isGrounded = false;
-        }
-    }
-
-    private void CheckGround()
-    {
-        Vector3 origin = _groundCheck.position;
-        _isGrounded = Physics.Raycast(origin, Vector3.down, _groundCheckDistance, _groundLayer);
-        //Debug.DrawRay(origin, Vector3.down * groundCheckDistance, Color.red);
+        _animator.SetFloat("Horizontal", (x+z!=0 ? Math.Abs(x+z) : Math.Abs(-x+z) ));
+        //_animator.SetFloat("Vertical", x);
     }
 
 }
