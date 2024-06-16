ce1256c455c9c8074d312992d9d166637e0989ca
Zaklzzzy <zaletin123@gmail.com>
2024-06-16 21:15:43 +0300

    feat: add outline and timer

--- Assets/Scripts/Objects/VanGarbage.cs
+++ Assets/Scripts/Objects/VanGarbage.cs
@@ -7,9 +7,15 @@ using UnityEngine;
 
 public class VanGarbage : NetworkBehaviour
 {
+    [Header("Counter")]
     [SerializeField] private TextMeshProUGUI _countText;
     [SerializeField] private int _maxCount;
     private int _count;
+    [Header("Timer")]
+    [SerializeField] private TextMeshProUGUI _timerText;
+    [SerializeField] private float _gameDuration = 300f;
+    private float _timeRemaining;
+    private bool _isGameOver = false;
 
     private HashSet<GameObject> objectsInTrigger = new HashSet<GameObject>();
 
@@ -18,11 +24,19 @@ public class VanGarbage : NetworkBehaviour
 
     private void Start()
     {
-        SetRandomRequiredObjects();
+        if (isServer)
+        {
+            SetRandomRequiredObjects();
+            RpcSetRandomRequiredObjects();
+        }
+
         _maxCount = FindObjectsByType<Dragable>(FindObjectsSortMode.None).Where(obj => obj.required == true).ToArray().Length;
         ChangeText();
 
         _roomManager = FindAnyObjectByType<NetworkRoomManager>();
+
+        _timeRemaining = _gameDuration;
+        StartCoroutine(GameTimer());
     }
 
     private void SetRandomRequiredObjects()
@@ -38,6 +52,16 @@ public class VanGarbage : NetworkBehaviour
         for (int i = 0; i < requiredCount; i++)
         {
             allDragableObjects[i].required = true;
+            EnableOutline(allDragableObjects[i].gameObject);
+        }
+    }
+
+    [ClientRpc]
+    private void RpcSetRandomRequiredObjects()
+    {
+        if (!isServer)
+        {
+            SetRandomRequiredObjects();
         }
     }
 
@@ -70,10 +94,15 @@ public class VanGarbage : NetworkBehaviour
             }
         }
     }
-
     private void ChangeText()
     {
         _countText.text = "Objects: " + _count + "/" + _maxCount;
+    }
+
+    [ClientRpc]
+    private void RpcChangeText()
+    {
+        ChangeText();
     }
 
     [Server]
@@ -93,6 +122,42 @@ public class VanGarbage : NetworkBehaviour
         if (sceneChanging)
         {
             _roomManager.ServerChangeScene("StartMenu");
+        }
+    }
+
+    [Server]
+    private IEnumerator GameTimer()
+    {
+        while (_timeRemaining > 0 && !_isGameOver)
+        {
+            yield return new WaitForSeconds(1f);
+            _timeRemaining--;
+
+            // Обновление UI таймера
+            RpcUpdateTimerText(_timeRemaining);
+
+            if (_timeRemaining <= 0)
+            {
+                _isGameOver = true;
+                StartCoroutine(ChangeSceneAfterDelay());
+            }
+        }
+    }
+
+    [ClientRpc]
+    private void RpcUpdateTimerText(float timeRemaining)
+    {
+        int minutes = Mathf.FloorToInt(timeRemaining / 60);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60);
+        _timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    private void EnableOutline(GameObject obj)
+    {
+        var outline = obj.GetComponent<Outline>();
+        if (outline != null)
+        {
+            outline.enabled = true;
         }
     }
 }
